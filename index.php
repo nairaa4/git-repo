9324a61502e5d8ddf3acddf1b3715665bd0ba65a
nairaa4 <nairadwiayusania@gmail.com>
2020-06-05 20:50:06 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Latihan Git Repo</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+        </script>
+        <script src="js/bootstrap.min.js"></script>
+        <style>
+        form{
+            margin-left:35%
+        }
+        </style>
+    </head>
+    <body> 
+    
+        <form method="post" action="cek_login.php">
+            
+
+            <div class="container">
+            <div class="col-md-5 col-md-offset-4" style="margin-top:25%">
+                <h3 align="center">Login</h3>
+                <br>
+                <div class="panel panel-default">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <input type="text" name="username" class="form-control input-sm" id="username" placeholder="Masukkan Username" required>
+                            </div>
+                        </div>
+
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <input type="password" name="password" class="form-control input-sm" id="password" placeholder="Masukkan Password" required>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-9">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            
+        </form>
+        
+    </body>
+</html>
